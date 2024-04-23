e0a134c4bacf08fe4749f658166958c91726ec6c
umuntfuNje <54364037+umuntfunje@users.noreply.github.com>
2024-04-23 04:13:10 +0200

    Add files via upload

---
+++ projects/Quiet-wam-controller.mk
@@ -0,0 +1,12 @@
+include ../config/Quiet-web.mk
+
+TARGET = ../build-web/scripts/Quiet-web.js
+
+SRC += $(WEB_SRC)
+CFLAGS += $(WEB_CFLAGS)
+CFLAGS += $(EXTRA_CFLAGS)
+LDFLAGS += $(WEB_LDFLAGS) \
+-s EXPORTED_FUNCTIONS=$(WEB_EXPORTS)
+
+$(TARGET): $(OBJECTS)
+	$(CC) $(CFLAGS) $(LDFLAGS) -o $@ $(SRC)
